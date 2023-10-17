978b6b72b6859895e5e146a91f6a2ff9a46e448e
pankx <a3427173515@163.com>
2023-10-17 21:06:25 +0800

    add : 测试 minio 的桶策略

--- src/test/java/com/kai/MinioUploadApplicationTests.java
+++ src/test/java/com/kai/MinioUploadApplicationTests.java
@@ -1,13 +1,24 @@
 package com.kai;
 
+import io.minio.GetBucketPolicyArgs;
+import io.minio.MinioClient;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MinioUploadApplicationTests {
 
+    @Autowired
+    MinioClient minioClient;
+
+    @SneakyThrows
     @Test
     void contextLoads() {
+
+        String bucketPolicy = minioClient.getBucketPolicy(GetBucketPolicyArgs.builder().bucket("minio-upload")
+                .build());
     }
 
 }
