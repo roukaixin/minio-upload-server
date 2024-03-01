32ed41de5b426b124ef8cc5fb79830e664b4216b
pankx <a3427173515@163.com>
2024-03-01 08:45:57 +0800

    refactor: 添加 swagger 注释

--- src/main/java/com/roukaixin/pojo/dto/FileInfoDTO.java
+++ src/main/java/com/roukaixin/pojo/dto/FileInfoDTO.java
@@ -1,5 +1,6 @@
 package com.roukaixin.pojo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,25 +19,30 @@ public class FileInfoDTO {
     /**
      * 文件MD5值
      */
+    @Schema(description = "文件 MD5 值")
     private String fileIdentifier;
 
     /**
      * 文件名字
      */
+    @Schema(description = "文件名")
     private String fileName;
 
     /**
      * 文件类型
      */
+    @Schema(description = "文件类型")
     private String fileType;
 
     /**
      * 文件大小总数
      */
+    @Schema(description = "文件大小。单位：字节(byte)")
     private Long totalSize;
 
     /**
      * 分片大小（单位：字节(byte)）
      */
+    @Schema(description = "平均分片大小。单位：字节(byte)")
     private Long chunkSize;
 }
